6f3bcf4545b2460abd303d0333ab59e2a39d553e
Looper <heeseon87@gmail.com>
2015-09-14 18:08:20 +0900

    First Commit

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>\d+)/result/$', views.results, name='results'),
-	url(r'^(?P<question_id>\d+)/votre/$', views.vote, name='vote'),
-)
+                       url(r'^$', views.index, name='index'),
+                       url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+                       url(r"^(?P<question_id>\d+)/result/$", views.results, name='results'),
+                       url(r'^(?P<question_id>\d+)/votre/$', views.vote, name='vote'),
+                       )
